9d80024dfac9a3c7356cccf0270b502ce6ccd393
brinapirc <brina.pirc8@gmail.com>
2019-11-05 23:32:36 +0100

    ustvarjena json in csv

--- podatki.py
+++ podatki.py
@@ -1,29 +1,41 @@
 import re
-
-with open('podatki-1.html', encoding='utf-8') as f:
-    vsebina_1 = f.read()
+import csv
+import json
+import os
+import sys
 
 with open('podatki-2.html', encoding='utf-8') as g:
     vsebina_2 = g.read()
 
-vsebina = vsebina_1 + vsebina_2
-
-vzorec = (
-    r'<a class="ranking-institution-title" href="/world-university-rankings/\S*" data-position="title" data-mz="">(?P<ime univerze>\D*.?)</a>' #ime univerze
-    r'<a href="/location/a4zw0{7}\D{3}\w\D{3}">(?P<država>\D*)</a>'                              #država
-    r'<td class="rank sorting_1 sorting_2">(?P<uvrstitev>(=?\d+|\d+–\d+|1001\+))</td>'                 #uvrstitev
-    r'<td class=" stats stats_number_students">(?P<število_vpisanih>\d+.\d{1,3})</td>'             #število vpisanih
-    r'<td class=" stats stats_pc_intl_students">(?P<procent_tujih_študentov>\d{0,2}%)</td>'            #procent tujih
-    r'<td class=" scores overall-score">(?P<overall>\d{2}.\d.*?)</td>'            #overall
-    r'<td class=" scores teaching-score">(?P<teaching>\d{2}\.\d)</td>'            #teaching
-    r'<td class=" scores research-score">(?P<research>\d+\.\d)</td>'              #research
-    r'<td class=" scores citations-score">(?P<citations>\d+\.\d)</td>'             #citations
-    r'<td class=" scores international_outlook-score">(?P<international_outlook>\d+\.\d)</td>' #international outlook
-    r'<td class=" scores industry_income-score">(?P<indutry_income>\d+\.\d)</td>'       #industry outcome
+vzorec_2 = (
+    r'<td class="rank">(?P<rank>(=?\d+|\d+–\d+|1001\+))</td>'
+    r'<td class="name namesearch sorting_1">'
+    r'<a class="ranking-institution-title" href="/world-university-rankings/\S*" data-position="title" data-mz="">(?P<name>\D*.?)</a><div class="location"><span>'
+    r'<a href="/location/a4zw0{7}\D{3}\w\D{3}">(?P<country>\D*)</a>'
+    r'</span></div></td>'
+    r'<td class=" scores overall-score">(?P<overall>\d{2}.\d.*?)</td>'
+    r'<td class=" scores teaching-score">(?P<teaching>\d{2}\.\d)</td>'
+    r'<td class=" scores research-score">(?P<research>.?\d+\.\d)</td>'
+    r'<td class=" scores citations-score">(?P<citations>\d+\.\d)</td>'
+    r'<td class=" scores industry_income-score">(?P<industry_income>\d+\.\d)</td>'
+    r'<td class=" scores international_outlook-score">(?P<international_outlook>\d+\.\d)</td>'
     )
 
-count = 0
-for zadetek in re.finditer(vzorec, vsebina):
-    print(zadetek.groupdict())
-    count += 1
-print(count)
+universities_2 = []
+d = 0
+
+for zadetek in re.finditer(vzorec_2, vsebina_2):
+    universities_2.append(zadetek.groupdict())
+    d += 1
+print(d)
+
+
+with open('universities_2.json', 'w', encoding='utf-8') as f:
+    json.dump(universities_2, f, indent=2)
+
+
+with open('universities_2.csv', 'w', encoding='utf-8') as csv_datoteka_2:
+    writer = csv.DictWriter(csv_datoteka_2, fieldnames=['rank', 'name', 'country', 'overall', 'teaching', 'research', 'citations', 'industry_income', 'international_outlook'])
+    writer.writeheader()
+    for slovar in universities_2:
+        writer.writerow(slovar)
